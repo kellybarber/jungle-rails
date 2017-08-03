644aca28e191acb85bdc1e5db39780064c8e6dc2
kellybarber <kelly.james.barber@gmail.com>
2017-08-03 14:38:27 +0000

    completed feature for admin creation of new categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,28 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+    @category = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    # render plain: params[:category].inspect
+
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Product created!'
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
